6f913376a2def001db4215cd609838af9002f330
Daniela Grossmann <daniela.grossmann@gmail.com>
2016-04-03 17:18:57 -0700

    Articles get saved to a User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331011526) do
+ActiveRecord::Schema.define(version: 20160404000256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 20160331011526) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "newspaper_id"
+  end
+
+  create_table "articles_users", force: true do |t|
+    t.integer "article_id"
+    t.integer "user_id"
   end
 
   create_table "newspapers", force: true do |t|
